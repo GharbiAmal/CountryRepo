4a0ad54a62aa9e91ccafda3f04c36c686dd4b04d
GharbiAmal <ghbi.amal@gmail.com>
2025-10-21 21:43:51 +0100

    Initial commit - country-service

---
+++ src/main/java/com/countryservice/demo/beans/Country.java
@@ -0,0 +1,57 @@
+package com.countryservice.demo.beans;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+
+@Table(name="country")
+public class Country {
+	@Id
+	@Column(name="id")
+	private int idCountry;
+	@Column(name="name_country")
+	private String name;
+	@Column(name="capital_name")
+	private String Capital;
+	
+	
+	 // --- Constructeur vide ---
+    public Country() {
+    }
+
+    // --- Constructeur avec tous les champs ---
+    public Country(int idCountry, String name, String capital) {
+        this.idCountry = idCountry;
+        this.name = name;
+        this.Capital = capital;
+    }
+
+	// --- Getters et Setters ---
+    public int getIdCountry() {
+        return idCountry;
+    }
+
+    public void setIdCountry(int idCountry) {
+        this.idCountry = idCountry;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCapital() {
+        return Capital;
+    }
+
+    public void setCapital(String capital) {
+        this.Capital = capital;
+    }
+
+}
